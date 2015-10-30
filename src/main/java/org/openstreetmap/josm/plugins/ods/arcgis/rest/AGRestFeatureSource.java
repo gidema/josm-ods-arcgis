045b2fe1c836910bf469cf9ab85f64ce7205280e
gidema <mail@gertjanidema.nl>
2015-10-30 17:01:23 +0100

    snapshot 0.6.4

--- src/main/java/org/openstreetmap/josm/plugins/ods/arcgis/rest/AGRestFeatureSource.java
+++ src/main/java/org/openstreetmap/josm/plugins/ods/arcgis/rest/AGRestFeatureSource.java
@@ -3,101 +3,101 @@ package org.openstreetmap.josm.plugins.ods.arcgis.rest;
 import java.io.IOException;
 
 import org.opengis.feature.type.FeatureType;
-import org.opengis.filter.Filter;
 import org.opengis.referencing.ReferenceIdentifier;
 import org.opengis.referencing.crs.CoordinateReferenceSystem;
 import org.openstreetmap.josm.plugins.ods.InitializationException;
-import org.openstreetmap.josm.plugins.ods.OdsDataSource;
 import org.openstreetmap.josm.plugins.ods.OdsFeatureSource;
 import org.openstreetmap.josm.plugins.ods.arcgis.rest.json.FeatureTypeParser;
 import org.openstreetmap.josm.plugins.ods.metadata.MetaData;
 
 public class AGRestFeatureSource implements OdsFeatureSource {
-  private boolean initialized = false;
-  private final AGRestHost host;
-  private final String feature;
-  private final Long featureId;
-  private FeatureType featureType;
-  private MetaData metaData;
+    private boolean initialized = false;
+    private final AGRestHost host;
+    private final String feature;
+    private final Long featureId;
+    private FeatureType featureType;
+    private MetaData metaData;
 
-  public AGRestFeatureSource(AGRestHost host, String feature) {
-    super();
-    this.host = host;
-    this.feature = feature;
-    String[] parts = feature.split("/");
-    //  this.featureName = parts[0];
-    this.featureId = Long.valueOf(parts[1]);
-  }
+    public AGRestFeatureSource(AGRestHost host, String feature) {
+        super();
+        this.host = host;
+        this.feature = feature;
+        String[] parts = feature.split("/");
+        // this.featureName = parts[0];
+        this.featureId = Long.valueOf(parts[1]);
+    }
 
-	@Override
-	public final AGRestHost getHost() {
-    return host;
-  }
+    @Override
+    public final AGRestHost getHost() {
+        return host;
+    }
 
-  @Override
-  public FeatureType getFeatureType() {
-    return featureType;
-  }
-  
-  
-  @Override
+    @Override
+    public FeatureType getFeatureType() {
+        return featureType;
+    }
+
+    @Override
     public String getIdAttribute() {
         return null;
     }
 
-@Override
-  public final String getFeatureName() {
-    return String.format("%s:%s", host.getName(), feature);
-  }
-
-  @Override
-  public void initialize() throws InitializationException {
-    if (initialized) return;
-    metaData = host.getMetaData();
-    HttpRequest request = new HttpRequest();
-    try {
-      request.open("GET", host.getUrl() + "/" + featureId);
-      request.addParameter("f", "json");
-      HttpResponse response = request.send();
-      FeatureTypeParser parser = new FeatureTypeParser();
-      featureType = parser.parse(response.getInputStream(), host.getName());
-      initialized = true;
-    } catch (IOException e) {
-      throw new InitializationException(e);
+    @Override
+    public final String getFeatureName() {
+        return String.format("%s:%s", host.getName(), feature);
     }
-  }
-  
-  @Override
-  public MetaData getMetaData() {
-    assert initialized;
-    return metaData;
-  }
 
-  public Long getFeatureId(){
-    return featureId;
-  }
-  
-  @Override
-  public CoordinateReferenceSystem getCrs() {
-    return featureType.getGeometryDescriptor().getCoordinateReferenceSystem();
-  }
+    @Override
+    public void initialize() throws InitializationException {
+        if (initialized)
+            return;
+        metaData = host.getMetaData();
+        HttpRequest request = new HttpRequest();
+        try {
+            request.open("GET", host.getUrl() + "/" + featureId);
+            request.addParameter("f", "json");
+            HttpResponse response = request.send();
+            FeatureTypeParser parser = new FeatureTypeParser();
+            featureType = parser.parse(response.getInputStream(),
+                    host.getName());
+            initialized = true;
+        } catch (IOException e) {
+            throw new InitializationException(e);
+        }
+    }
 
-  @Override
-  public String getSRS() {
-    ReferenceIdentifier rid = getCrs().getIdentifiers().iterator().next();
-    return rid.toString();
-  }
-  
-  @Override
-  public Long getSRID() {
-    ReferenceIdentifier rid = getCrs().getIdentifiers().iterator().next();
-    return Long.parseLong(rid.getCode());
-  }
-    
-  @Override
-  public OdsDataSource newDataSource(Filter filter) {
-    return new AGRestDataSource(this);
-  }
+    @Override
+    public MetaData getMetaData() {
+        assert initialized;
+        return metaData;
+    }
+
+    public Long getFeatureId() {
+        return featureId;
+    }
+
+    @Override
+    public CoordinateReferenceSystem getCrs() {
+        return featureType.getGeometryDescriptor()
+                .getCoordinateReferenceSystem();
+    }
+
+    @Override
+    public String getSRS() {
+        ReferenceIdentifier rid = getCrs().getIdentifiers().iterator().next();
+        return rid.toString();
+    }
+
+    @Override
+    public Long getSRID() {
+        ReferenceIdentifier rid = getCrs().getIdentifiers().iterator().next();
+        return Long.parseLong(rid.getCode());
+    }
+
+//    @Override
+//    public OdsDataSource newDataSource(Filter filter) {
+//        return new AGRestDataSource(this);
+//    }
 
     // @Override
     // public SimpleFeatureSource getFeatureSource() {
